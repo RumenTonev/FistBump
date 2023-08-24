d62a6b33b597438e32fef823226c8f59ccbe846f
RumenTonev <rumen.tonev@yahoo.com>
2023-08-24 22:35:40 +0300

    switch to .js

---
+++ FistBump/views/ProfileView.jsx
@@ -0,0 +1,14 @@
+import { Button, View } from "react-native";
+
+import React, { FC } from 'react';
+export const ProfileView = ({navigation}) => {
+    return(<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            
+    <Button
+  title="Go to Settings"
+  onPress={() =>
+   navigation.navigate('Settings')
+  }
+  />
+  </View>)
+  };
