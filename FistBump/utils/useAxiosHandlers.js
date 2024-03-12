6e535dd3996d85a75c660ffd8aa5ad748ca6422b
RumenTonev <rumen.tonev@yahoo.com>
2024-03-12 22:33:26 +0200

    Make testing path and change ios logo

--- FistBump/utils/useAxiosHandlers.js
+++ FistBump/utils/useAxiosHandlers.js
@@ -6,9 +6,10 @@ import axios from "axios";
 import { getGetRequest, getPostRequestObject } from "./axiosRepo";
 import { useDbHandlers } from "./useDbHandlers";
 import { setConfirmedLogin, setLoggedIn } from "../store/userSlice";
-import { useDispatch ,useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigation } from '@react-navigation/native';
 import { Alert } from "react-native";
+import Config from "react-native-config";
 
 
 // "account_id": "72e47e47-95b9-41c2-bdcd-55dd9bb04e14",
@@ -19,18 +20,25 @@ import { Alert } from "react-native";
 // "expire_date": "2022-03-04 14:50:57"
 
 export function useAxiosHandlers() {
-  const{handleGet}=useDbHandlers()
+  const { handleGet } = useDbHandlers()
   const user = useSelector((state) => state.user.user);
-  const {id,isUs}=user
+  const { id, isUs } = user
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
   const handleConfirmOTP = useCallback(async (code) => {
     console.log('CONFIRMOTPSTART')
-  let localPhone=id
-  
-  if(id.startsWith('+'))localPhone=id.substring(1)
+    let localPhone = id
 
+    if (id.startsWith('+')) localPhone = id.substring(1)
+    console.log('CODE '+code)
+    console.log('SETCODE '+Config.REACT_APP_LOGIN_CODE)
+    if (code == Config.REACT_APP_LOGIN_CODE) {
+      dispatch(setConfirmedLogin())
+      handleGet(id, isUs)
+      navigation.navigate('Landing')
+      return
+    }
     const config = getGetRequest(localPhone, code)
     axios(config)
       .then(async function (response) {
@@ -38,43 +46,51 @@ export function useAxiosHandlers() {
         if (response.data) {
 
           const { data } = response.data
-          if (data){
+          if (data) {
             if (data.length > 0) {
               const { code: savedcode } = data[0]
               if (savedcode == code) {
                 dispatch(setConfirmedLogin())
-                handleGet(id,isUs)
+                handleGet(id, isUs)
                 navigation.navigate('Landing')
               }
             }
           }
         }
-//throw error pop-up Invalid code and stay on the same page
-              
-        })
+        //throw error pop-up Invalid code and stay on the same page
+
+      })
       .catch(function (error) {
         console.log('HANDLECONFIRMOTPERROR')
         console.log(error);
-        
+
       });
 
-  }, [id,isUs,navigation])
+  }, [id, isUs, navigation])
 
 
 
 
-  const handleSendOTP = useCallback(async (phoneNumber,isUs) => {
-  const flag=false
-  
-  if(flag) return
+  const handleSendOTP = useCallback(async (phoneNumber, isUs) => {
+    const flag = false
+    console.log('PHONENUMBER ' + phoneNumber)
+    console.log('CONFIGPHONENUMBER ' + Config.REACT_APP_LOGIN_PHONE)
+    if(phoneNumber==Config.REACT_APP_LOGIN_PHONE)
+    {
+      dispatch(setLoggedIn({ phone: phoneNumber, isUs: isUs }))
+
+      navigation.navigate('ConfirmationCode')
+      return
+    }
+    if (flag) return
     console.log('IIIIIIII')
     const config = getPostRequestObject(phoneNumber)
     axios(config)
       .then(async function (response) {
         if (response.data) {
-          const {data}=response
-          dispatch(setLoggedIn({ phone: data.data.phone ,isUs:isUs}))
-          
+          const { data } = response
+          dispatch(setLoggedIn({ phone: data.data.phone, isUs: isUs }))
+
           navigation.navigate('ConfirmationCode')
         }
         else {
@@ -82,7 +98,7 @@ export function useAxiosHandlers() {
         }
       })
       .catch(function (error) {
-        if(error.code=='ERR_NETWORK')Alert.alert('Network problem');
+        if (error.code == 'ERR_NETWORK') Alert.alert('Network problem');
         console.log(error);
       });
 
